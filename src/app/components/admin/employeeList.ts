6c12517dfbc30a8d1c76c38a5f527ca4602b09ea
ygbekou <ericgbekou@hotmail.com>
2019-05-18 01:02:17 -0500

    More changes for Employee

--- src/app/components/admin/employeeList.ts
+++ src/app/components/admin/employeeList.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy, ChangeDetectorRef, ViewChild } from '@angular/core';
 import { Router, ActivatedRoute, NavigationExtras } from '@angular/router';
 import { Message } from 'primeng/api';
-import { GenericService } from '../../services';
+import { GenericService, GlobalEventsManager } from '../../services';
 import { Employee, User, SearchCriteria } from '../../models';
 import { DepartmentDropdown } from '../dropdowns';
 import { TranslateService, LangChangeEvent} from '@ngx-translate/core';
@@ -13,26 +13,27 @@ import { Constants } from 'src/app/app.constants';
   providers: [GenericService, DepartmentDropdown]
 })
 export class EmployeeList implements OnInit, OnDestroy {
-  
+
   messages: Message[] = [];
   employees: Employee[] = [];
   cols: any[];
   searchCriteria: SearchCriteria = new SearchCriteria();
-  
+
   constructor
     (
     private genericService: GenericService,
     private translate: TranslateService,
+    public globalEventsManager: GlobalEventsManager,
     public departmentDropdown: DepartmentDropdown,
     private router: Router,
     ) {
 
-    
+
   }
-  
+
   updateCols() {
-    for (var index in this.cols) {
-      let col = this.cols[index];
+    for (let index in this.cols) {
+      const col = this.cols[index];
       this.translate.get(col.headerKey).subscribe((res: string) => {
         col.header = res;
       });
@@ -41,69 +42,75 @@ export class EmployeeList implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.cols = [
-            { field: 'lastName', header: 'Last Name', headerKey: 'COMMON.LAST_NAME', type:'user' },
-            { field: 'firstName', header: 'First Name', headerKey: 'COMMON.FIRST_NAME', type:'user' },
-            { field: 'departmentName', header: 'Department', headerKey: 'COMMON.DEPARTMENT', type:'department' },
-            { field: 'groupName', header: 'Role', headerKey: 'COMMON.ROLE' },
-            { field: 'email', header: 'Email', headerKey: 'COMMON.E_MAIL', type:'user' },
-            { field: 'phone', header: 'Phone', headerKey: 'COMMON.PHONE', type:'user' },
-            { field: 'sex', header: 'Gender', headerKey: 'COMMON.GENDER', type:'user' },
-            { field: 'status', header: 'Status', headerKey: 'COMMON.STATUS', type:'string' }
+            { field: 'lastName', header: 'Last Name', headerKey: 'COMMON.LAST_NAME', type: 'user', 
+                        style: {width: '15%', 'text-align': 'center'} },
+            { field: 'firstName', header: 'First Name', headerKey: 'COMMON.FIRST_NAME', type: 'user', 
+                        style: {width: '15%', 'text-align': 'center'} },
+            { field: 'departmentName', header: 'Department', headerKey: 'COMMON.DEPARTMENT', type: 'department',
+                        style: {width: '10%', 'text-align': 'center'} },
+            { field: 'groupName', header: 'Role', headerKey: 'COMMON.ROLE',
+                        style: {width: '10%', 'text-align': 'center'} },
+            { field: 'email', header: 'Email', headerKey: 'COMMON.E_MAIL', type: 'user',
+                        style: {width: '15%', 'text-align': 'center'} },
+            { field: 'homePhone', header: 'Phone', headerKey: 'COMMON.PHONE',
+                        style: {width: '15%', 'text-align': 'center'} },
+            { field: 'sex', header: 'Gender', headerKey: 'COMMON.GENDER', type: 'user',
+                        style: {width: '5%', 'text-align': 'center'} },
+            { field: 'status', header: 'Status', headerKey: 'COMMON.STATUS', type: 'string',
+                        style: {width: '5%', 'text-align': 'center'} }
         ];
-    
+
     this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
         this.updateCols();
     });
   }
- 
-  
+
+
   ngOnDestroy() {
     this.employees = null;
   }
-  
-  edit(employeeId : number) {
+
+  edit(employeeId: number) {
     try {
-      let navigationExtras: NavigationExtras = {
+      const navigationExtras: NavigationExtras = {
         queryParams: {
-          "employeeId": employeeId,
+          'employeeId': employeeId,
         }
-      }
-      this.router.navigate(["/admin/employeeDetails"], navigationExtras);
-    }
-    catch (e) {
+      };
+      this.router.navigate(['/admin/employeeDetails'], navigationExtras);
+    } catch (e) {
       console.log(e);
     }
   }
 
-  delete(employeeId : number) {
+  delete(employeeId: number) {
     try {
-      let navigationExtras: NavigationExtras = {
+      const navigationExtras: NavigationExtras = {
         queryParams: {
-          "employeeId": employeeId,
+          'employeeId': employeeId,
         }
-      }
-      this.router.navigate(["/admin/employeeDetails"], navigationExtras);
-    }
-    catch (e) {
+      };
+      this.router.navigate(['/admin/employeeDetails'], navigationExtras);
+    } catch (e) {
       console.log(e);
     }
   }
 
   search() {
-   
-        let parameters: string [] = []; 
-            
+
+        const parameters: string [] = [];
+
         if (this.searchCriteria.lastName != null && this.searchCriteria.lastName.length > 0)  {
-          parameters.push('e.user.lastName like |lastName|' + '%' + this.searchCriteria.lastName + '%' + '|String')
+          parameters.push('e.user.lastName like |lastName|' + '%' + this.searchCriteria.lastName + '%' + '|String');
         }
         if (this.searchCriteria.firstName != null && this.searchCriteria.firstName.length > 0)  {
-          parameters.push('e.user.firstName like |firstName|' + '%' + this.searchCriteria.firstName + '%' + '|String')
-        } 
+          parameters.push('e.user.firstName like |firstName|' + '%' + this.searchCriteria.firstName + '%' + '|String');
+        }
         if (this.searchCriteria.department != null && this.searchCriteria.department.id > 0)  {
-          parameters.push('e.department.id = |departmentId|' + this.searchCriteria.department.id + '|Long')
-        }  
-        
-        if (parameters.length === 1) {
+          parameters.push('e.department.id = |departmentId|' + this.searchCriteria.department.id + '|Long');
+        }
+
+        if (parameters.length === 0) {
             this.translate.get(['MESSAGE.NO_CRITERIA_SET', 'COMMON.SEARCH']).subscribe(res => {
               this.messages.push({
                 severity: Constants.ERROR, summary: res['COMMON.SEARCH'],
@@ -128,7 +135,7 @@ export class EmployeeList implements OnInit, OnDestroy {
       statusDesc = 'Actif';
     } else if (employee.status === 1) {
       statusDesc = 'Inactif';
-    } 
-    return statusDesc; 
+    }
+    return statusDesc;
   }
  }
