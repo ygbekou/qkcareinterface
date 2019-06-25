457c17b3458f5ff1ef87a2a8531fb44439f54656
Panawe Batanado <panawe@gmail.com>
2019-06-25 01:53:44 -0400

    Change

--- src/app/app.rightpanel.component.ts
+++ src/app/app.rightpanel.component.ts
@@ -10,7 +10,7 @@ import { TokenStorage } from './services';
 export class AppRightPanelComponent implements AfterViewInit {
 
 	@ViewChild('scrollRightPanel') rightPanelMenuScrollerViewChild: ScrollPanel;
-
+	public activeTab = 0;
 	constructor(public app: AppComponent,
 		public tokenStorage: TokenStorage,
 		private router: Router) { }
@@ -20,8 +20,20 @@ export class AppRightPanelComponent implements AfterViewInit {
 		setTimeout(() => { this.rightPanelMenuScrollerViewChild.moveBar(); }, 100);
 	}
 
-	onTabChange(event) {
-		setTimeout(() => { this.rightPanelMenuScrollerViewChild.moveBar(); }, 450);
+	onTabChange(evt) {
+
+		setTimeout(() => {
+			this.rightPanelMenuScrollerViewChild.moveBar();
+			this.activeTab = evt.index;
+			if (evt.index === 0) {
+				this.activeTab = 0;
+			} else if (evt.index === 1) {
+				this.activeTab = 1;
+			} else {
+				this.activeTab = 1;
+			}
+
+		}, 450);
 	}
 
 	logOut() {
@@ -30,7 +42,4 @@ export class AppRightPanelComponent implements AfterViewInit {
 		window.location.reload();
 	}
 
-	onTabChange(){
-		
-	}
 }
